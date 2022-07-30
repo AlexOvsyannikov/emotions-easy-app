706d027c8835319bfb176329c5d1667b05ffa012
Alexandr Ovsyannikov <alexov2004@gmail.com>
2022-07-30 03:16:49 +0300

    adding sentiment analysis and main route

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from flask import Flask, request
-from modeule import get_emotions
+from modeule import get_emotions, sentiment_prediction
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', "POST"])
-def hello_world():  # put application's code here
+@app.route('/', methods=['GET'])
+def main():  # put application's code here
+    return 'THIS IS JUST LEARNING PROJECT TO PASS AN IBM PROFESSIONAL CERTIFICATE.\n ' \
+           'I FACED A PROBLEM WITH AN IBM ACCOUNT SO I HAVE TO WRITE OWN WEB APIs TO FINISH A COURSE.\n ' \
+           '@AlexOFF'
+
+
+@app.route('/emotion', methods=['GET', "POST"])
+def emotion():  # put application's code here
     if request.method == 'GET':
         return get_emotions(text=request.args.get('text', 'NoNe'))
     else:
         return get_emotions(text=request.form.get('text', 'NoNe'))
+
+
+@app.route('/sentiment', methods=['GET', "POST"])
+def sentiment():  # put application's code here
+    if request.method == 'GET':
+        return sentiment_prediction(text=request.args.get('text', 'NoNe'))
+    else:
+        return sentiment_prediction(text=request.form.get('text', 'NoNe'))
 
 
 if __name__ == '__main__':
--- modeule.py
+++ modeule.py
@@ -1,5 +1,7 @@
 import nltk
 from LeXmo import LeXmo
+from nltk.sentiment import SentimentIntensityAnalyzer
+from textblob import TextBlob
 
 nltk.download('punkt')
 
@@ -8,3 +10,15 @@ def get_emotions(text):
     emo = LeXmo.LeXmo(text)
     emo.pop('text', None)
     return emo
+
+
+def sentiment_prediction(x):
+    _sc = TextBlob(x).sentiment.polarity
+    if _sc > 0.2:
+        return {'sentiment': 'positive'}
+    if -0.1 < _sc < 0.2:
+        return {'sentiment': 'neutral'}
+    else:
+        return {'sentiment': 'negative'}
+
+
